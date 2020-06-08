45943d1b5479e7b65c05aca1b82e4ec37b7de7d5
AymenRaouf <ga_bazouzi@esi.dz>
2020-06-08 22:39:36 +0100

    added dtaa and data reader

---
+++ data.py
@@ -0,0 +1,9 @@
+import numpy as np
+def read(path):
+    return np.loadtxt(path, dtype='i', max_rows=5)
+    
+    
+if __name__ == '__main__':
+    path = './data/ta20_5.txt'
+    l = read(path)
+    print(l)
