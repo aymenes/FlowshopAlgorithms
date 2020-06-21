807032cbc98cd94460f18bbd5fb64e6abfd07ccc
ramziChbl <gr_chikhbled@esi.dz>
2020-06-21 17:50:56 +0100

    tested size population variation

--- testAlgoGenetic/testAG.py
+++ testAlgoGenetic/testAG.py
@@ -25,7 +25,7 @@ def varier_taille_population():
 	nbIterPop80 = []
 	nbIterPop100 = []
 
-	for _ in range(1, 10):
+	for _ in range(1, 9):
 		x.append(_)
 		path = './data/ta20_20_{}.txt'.format(_)
 		matrice = dataReader.read(path, 20)
@@ -35,10 +35,11 @@ def varier_taille_population():
 		machine_count, job_count = matrice.shape
 		print('iteration : {}'.format(_))
 		print('{}:{}'.format(machine_count, job_count))
-		initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
+		#initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
 
 		# POP30
 		population_size = 30
+		initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
 		start = time()
 		result = genetic(matrice, initPop, population_size, 0.1, 200, limit=0)
 		print('  Ordre : {}'.format(result[0]))
@@ -52,6 +53,7 @@ def varier_taille_population():
 
 		# POP50
 		population_size = 50
+		initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
 		start = time()
 		result = genetic(matrice, initPop, population_size, 0.1, 200, limit=0)
 		print('  Ordre : {}'.format(result[0]))
@@ -65,6 +67,7 @@ def varier_taille_population():
 
 		# POP80
 		population_size = 80
+		initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
 		start = time()
 		result = genetic(matrice, initPop, population_size, 0.1, 200, limit=0)
 		print('  Ordre : {}'.format(result[0]))
@@ -78,6 +81,7 @@ def varier_taille_population():
 
 		# POP100
 		population_size = 100
+		initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
 		start = time()
 		result = genetic(matrice, initPop, population_size, 0.1, 200, limit=0)
 		print('  Ordre : {}'.format(result[0]))
@@ -116,84 +120,4 @@ def varier_taille_population():
 	plt.legend()
 	plt.show()
 
-
-
-
-path = './data/ta20_20.txt'
-matrice = dataReader.read(path, 20)
-
-matrice = np.array(matrice)
-
-population_size = 50
-job_count = 20
-
-# for stat
-x = [x for x in range(1, 6)]
-tempsAG = []
-tempsHY = []
-makespanAG = []
-makespanHY = []
-nbIterAG = []
-nbIterHY = []
-
-for _ in range(1):
-
-	print('Algorithme NEH :')
-	start = time()
-	result = neh(matrice)
-	end = time()
-	print('  Ordre : {}'.format(result[0]))
-	print('  Makespan : {}'.format(result[1]))
-	print('  Temps d\'execution : {:.6}s'.format(end - start))
-
-	# Generer une les individus de la population aleatoirement
-	#initPop = [sample(list(range(1, job_count + 1)), job_count) for _ in range(0, population_size)] # same repeated individual
-	initPop = [sample(list(range(0, job_count)), job_count) for _ in range(0, population_size)] # same repeated individual
-
-	print('Algorithme Hybride :')
-	start = time()
-	#result = genetic(matrice, initPop, 100, 0.1, 200, limit=2400)
-	result = genetic_rt(matrice, initPop, population_size, 0.1, 200, limit=2400)
-	print('  Ordre : {}'.format(result[0]))
-	print('  Makespan : {}'.format(result[1]))
-	end = time()
-	print('  Temps d\'execution : {:.6}s'.format(end - start))
-
-	tempsHY.append(end - start)
-	makespanHY.append(result[1])
-	nbIterHY.append(result[2])
-
-	print('Algorithme Genetique :')
-	start = time()
-	result = genetic(matrice, initPop, population_size, 0.1, 200, limit=2400)
-	print('  Ordre : {}'.format(result[0]))
-	print('  Makespan : {}'.format(result[1]))
-	end = time()
-	print('  Temps d\'execution : {:.6}s'.format(end - start))
-	tempsAG.append(end - start)
-	makespanAG.append(result[1])
-	nbIterAG.append(result[2])
-	print()
-
-
-plt.plot(x, makespanAG, label='AG')
-plt.plot(x, makespanHY, label='HY')
-plt.xlabel('Test')
-plt.ylabel('Makespan')
-plt.legend()
-plt.show()
-
-
-plt.plot(x, tempsAG, label='AG')
-plt.plot(x, tempsHY, label='HY')
-plt.xlabel('Test')
-plt.ylabel('Temps d\'execution')
-plt.legend()
-plt.show()
-
-plt.plot(x, nbIterAG, label='AG')
-plt.plot(x, nbIterHY, label='HY')
-plt.xlabel('Test')
-plt.ylabel('Nombre d\'iterations')
-plt.legend()
-plt.show()
+varier_taille_population()
